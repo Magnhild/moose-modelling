f0a67c11408e0c49362844a4056c73d98237861b
Magnhild Sletten <magsletten@gmail.com>
2017-04-05 10:21:30 +0100

    tried the last few ways to get the glmer to work without success. Now we abandon it and use the glm

--- Glmer with coordinates.R
+++ Glmer with coordinates.R
@@ -33,6 +33,7 @@ altitude <- (first_data$Meters_above_sea_level)
 
 library(lme4)
 library(MuMIn)
+library(arm)
 IND <- factor(1:nrow(first_data))
 
 #redderr is rank deficient so we drop it. Model then runs without error. 
@@ -98,8 +99,104 @@ altitude <- scale(altitude)
 mixed.model4 <- glmer(moose ~ area + x + roedeer + biomass + psy + FSAV + veg + class + bilberry + dominant + acc + accpsy + species + trees + road + house + altitude + (1|IND) + (1|quadrat), data=first_data, family = poisson)
                    
 #nothing. Now check for highly correlated variables   
-anova(mixed.model3)
+#x, y, and altitude, and psy and biomass are highly correlated (rs>0.8)
+#so I try to remove some of them from the model
 
-summary(mixed.model3)
+mixed.model5 <- glmer(moose ~ y + area + roedeer + biomass + FSAV + veg + class + bilberry + dominant + acc + accpsy + species + trees + road + house + (1|IND) + (1|quadrat), data=first_data, family = poisson)
 
-r.squaredGLMM(mixed.model3)
+#no combination of that worked 
+#now i try to standardise the model
+#to do that I need a functioning model, so I remove x + y and try again
+#I also remove class
+mixed.model6 <- glmer(moose ~ area + x + y + roedeer + biomass + psy + FSAV + hunting + veg + bilberry + wolf + dominant + acc + accpsy + species + trees + road + house + altitude + (1|IND) + (1|quadrat), data=first_data, family = poisson)
+
+stdz.model <- standardize(mixed.model6, standardize.y = FALSE)
+#not converging, so i try to automate model selection
+model.set <- dredge(stdz.model) 
+
+#this didnt work. I am going to try and rescale the predictor variables again, but using a different method 
+#never mind, that didnt work. Now I am increasing the amount of iterations
+mixed.model7 <- glmer(moose ~ area + roedeer + biomass + psy + FSAV + hunting + veg + 
+                        bilberry + dominant + acc + accpsy + species + trees + road + house + 
+                        altitude + (1|IND) + (1|quadrat), data=first_data, family = poisson, 
+                      glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 200000)))
+
+#bear in mind these are currently scaled 
+#so that actaully worked however Hessian is singular and it is therefore probably a false positive.
+#Now I am going to try it with a different optimiser (not bobyqa)
+
+
+mixed.model8 <- glmer(moose ~ area + roedeer + biomass + psy + FSAV + hunting + veg + 
+                        bilberry + dominant + acc + accpsy + species + trees + road + house + 
+                        altitude + (1|IND) + (1|quadrat), data=first_data, family = poisson, 
+                      glmerControl(optCtrl = list(maxfun = 2e5)))
+
+#so that worked in approximately a quarter of the time, but the convergence warning is back
+#so I guess we can't keep it
+
+#I have now descaled the continuous parameters, and I am giving mm7 one more go. 
+
+mixed.model9 <- glmer(moose ~ area + roedeer + biomass + psy + FSAV + hunting + veg + 
+                        bilberry + dominant + acc + accpsy + species + trees + road + house + 
+                        altitude + (1|IND) + (1|quadrat), data=first_data, family = poisson, 
+                      glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 200000)))
+
+#that failed to converge. so we are working with SCALED parameters and 200 000 iterations. 
+
+#Now I try to double check the gradient calculations
+derivs1 <- mixed.model7@optinfo$derivs
+sc_grad1 <- with(derivs1,solve(Hessian,gradient))
+max(abs(sc_grad1))
+
+max(pmin(abs(sc_grad1),abs(derivs1$gradient)))
+#this is getting close to the tolerance we usually set (0.001). 
+#bear in mind that I have absolutely no idea what that means
+
+#now I am going to give standardisation another go
+#but this time without the x and y variables
+mixed.model10 <- glmer(moose ~ area + roedeer + biomass + psy + FSAV + hunting + veg + bilberry + wolf + dominant + acc + accpsy + species + trees + road + house + altitude + (1|IND) + (1|quadrat), data=first_data, family = poisson)
+
+stdz.model <- standardize(mixed.model7, standardize.y = FALSE)
+#same problems as mm7
+#now i try to transform moose, see if that gives us more variation to work with
+logmoose <- (moose)^3
+
+mixed.model11 <- glmer(logmoose ~ area + roedeer + biomass + psy + FSAV + hunting + veg + bilberry + wolf + 
+                         dominant + acc + accpsy + species + trees + road + house + altitude + 
+                         (1|IND) + (1|quadrat), data=first_data, family = poisson)
+
+#warnings of convergense and singluar hessian
+#now we try it on the 200000 iterations
+
+mixed.model12 <- glmer(logmoose ~ area + roedeer + biomass + psy + FSAV + hunting + veg + 
+                        bilberry + dominant + acc + accpsy + species + trees + road + house + 
+                        altitude + (1|IND) + (1|quadrat), data=first_data, family = poisson, 
+                      glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 200000)))
+
+#that didnt really work
+#now I try 200 000 iterations without the obvious variables
+mixed.model12 <- glmer(moose ~ biomass + FSAV + veg + 
+                         bilberry + dominant + acc + accpsy + species + trees + road + 
+                         altitude + (1|IND) + (1|quadrat), data=first_data, family = poisson, 
+                       glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 200000)))
+
+#now theres a negative hessian instead of a singular one
+#I try the same modela s above but with x and y coordinates
+mixed.model13 <- glmer(moose ~ biomass + x + y + FSAV + veg + 
+                         bilberry + dominant + acc + accpsy + species + trees + road + 
+                         altitude + (1|IND) + (1|quadrat), data=first_data, family = poisson, 
+                       glmerControl(optimizer = "bobyqa", optCtrl = list(maxfun = 200000)))
+
+#that still doesnt work
+#now i try it with 200 000 iterations but without bobyqa 
+
+mixed.model13 <- glmer(moose ~ biomass + FSAV + veg + 
+                         bilberry + dominant + acc + accpsy + species + trees + road + 
+                         altitude + (1|IND) + (1|quadrat), data=first_data, family = poisson, 
+                       glmerControl(optCtrl = list(maxfun = 200000)))
+
+anova(mixed.model12)
+
+summary(mixed.model1)
+
+r.squaredGLMM(mixed.model12)
